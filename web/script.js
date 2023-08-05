0f0f4b4d60583fe481bc65a91d7b7637471cf6a8
Janek <development@superyu.xyz>
2023-08-05 06:05:02 +0200

    Updated webui
    Uses a bounding box to zoom in without cropping out any radar entities

--- web/script.js
+++ web/script.js
@@ -5,6 +5,9 @@ const enemyColor = "#ec040b"
 const bombColor = "#eda338"
 const textColor = "#d1d1d1"
 
+// Should zoom or not
+shouldZoom = true
+
 // Common
 canvas = null
 ctx = null
@@ -17,23 +20,122 @@ loaded = false
 entityData = null
 update = false
 
+/// Radarflow zoom in
+zoomSet = false
+safetyBound = 50
+boundingRect = null
+
 // networking
 websocket = null
 websocketAddr = `ws://${window.location.hostname}:8001`
 
-// Util function
-function degreesToRadians (degrees) {
-    return degrees * (Math.PI/180);     
+// Util functions
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+const degreesToRadians = (degrees) => degrees * (Math.PI/180);
+function makeBoundingRect(x1, y1, x2, y2, aspectRatio) {
+    const topLeftX = x1;
+    const topLeftY = y1;
+    const bottomRightX = x2;
+    const bottomRightY = y2;
+
+    const width = bottomRightX - topLeftX;
+    const height = bottomRightY - topLeftY;
+
+    let newWidth, newHeight;
+    if (width / height > aspectRatio) {
+        // Wider rectangle
+        newHeight = width / aspectRatio;
+        newWidth = width;
+    } else {
+        // Taller rectangle
+        newWidth = height * aspectRatio;
+        newHeight = height;
+    }
+
+    const centerX = (topLeftX + bottomRightX) / 2;
+    const centerY = (topLeftY + bottomRightY) / 2;
+
+    const rectMinX = centerX - newWidth / 2;
+    const rectMaxX = centerX + newWidth / 2;
+    const rectMinY = centerY - newHeight / 2;
+    const rectMaxY = centerY + newHeight / 2;
+
+    const boundingRectangle = {
+        x: rectMinX,
+        y: rectMinY,
+        width: rectMaxX - rectMinX,
+        height: rectMaxY - rectMinY,
+    }
+
+    const boundingRectangle2 = {
+        x: 0,
+        y: 0,
+        width: image.width / 1.2,
+        height: image.width / 1.2,
+    }
+
+    return boundingRectangle;
+}
+
+function boundingCoordinates(coordinates, boundingRect) {
+    const xScale = boundingRect.width / image.width;
+    const yScale = boundingRect.height / image.height;
+
+    const newX = (coordinates.x - boundingRect.x) / xScale;
+    const newY = (coordinates.y - boundingRect.y) / yScale;
+
+    return {x: newX, y: newY};
+}
+
+function boundingScale(value, boundingRect) {
+    const scale = image.width / boundingRect.width;
+    return value * scale
+}
+
+function mapCoordinates(coordinates) {
+    let offset_x = coordinates.x - map.pos_x;
+    let offset_y = coordinates.y - map.pos_y;
+
+    offset_x /= map.scale;
+    offset_y /= -map.scale;
+
+    return {x: offset_x, y: offset_y}
 }
 
 function render() {
     if (update) {
         if (loaded) {
             update = false
+
+            // Iterate through the array and update the min/max values
+            if (entityData != null && map != null && shouldZoom) {
+
+                let minX = Infinity
+                let minY = Infinity
+                let maxX = -Infinity
+                let maxY = -Infinity
+
+                entityData.forEach((data) => {
+                    let mapCords = mapCoordinates(data.Player.pos)
+    
+                    minX = Math.min(minX, mapCords.x);
+                    minY = Math.min(minY, mapCords.y);
+                    maxX = Math.max(maxX, mapCords.x);
+                    maxY = Math.max(maxY, mapCords.y);
+                });
+
+
+                boundingRect = makeBoundingRect(minX-safetyBound, minY-safetyBound, maxX+safetyBound, maxY+safetyBound, image.width/image.height)
+
+                zoomSet = true
+            } else if (zoomSet) {
+                zoomSet = false
+            }
+
             drawImage()
 
             if (entityData != null) {
-                entityData.forEach(function(data) {
+                entityData.forEach((data) => {
                     if (data.Bomb !== undefined) {
                         drawBomb(data.Bomb.pos.x, data.Bomb.pos.y, data.Bomb.isPlanted)
                     } else {
@@ -50,8 +152,7 @@ function render() {
                         }
 
                         drawEntity(
-                            data.Player.pos.x, 
-                            data.Player.pos.y, 
+                            data.Player.pos,
                             fillStyle, 
                             data.Player.isDormant,
                             data.Player.hasBomb,
@@ -82,22 +183,30 @@ function render() {
 }
 
 function drawImage() {
-    if (image != null && canvas != null)
+    if (image == null || canvas == null)
+        return
+
+    if (zoomSet != false && boundingRect.x != null) {
+        ctx.drawImage(image, boundingRect.x, boundingRect.y, boundingRect.width, boundingRect.height, 0, 0, canvas.width, canvas.height)
+    } else {
         ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height)
+    }
 }
 
-function drawBomb(x, y, planted) {
+function drawBomb(pos, planted) {
     if (map == null)
         return
 
-    let offset_x = x - map.pos_x;
-    let offset_y = y - map.pos_y;
-
-    offset_x /= map.scale;
-    offset_y /= -map.scale;
+    if (zoomSet) {
+        pos = boundingCoordinates(mapCoordinates(pos), boundingRect)
+        size = boundingScale(5, boundingRect);
+    } else {
+        pos = mapCoordinates(pos)
+        size = 5
+    }
 
     ctx.beginPath();
-    ctx.arc(offset_x, offset_y, 5, 0, 2 * Math.PI);
+    ctx.arc(pos.x, pos.y, size, 0, 2 * Math.PI);
     ctx.fillStyle = bombColor;
     ctx.fill();
     ctx.closePath();
@@ -109,34 +218,40 @@ function drawBomb(x, y, planted) {
     }
 }
 
-function drawEntity(x, y, fillStyle, dormant, hasBomb, yaw) {
+function drawEntity(pos, fillStyle, dormant, hasBomb, yaw) {
     if (map == null)
         return
 
-    const circleRadius = 7
-
-    let offset_x = x - map.pos_x;
-    let offset_y = y - map.pos_y;
-
-    offset_x /= map.scale;
-    offset_y /= -map.scale;
+    if (zoomSet) {
+        pos = boundingCoordinates(mapCoordinates(pos), boundingRect)
+        circleRadius = boundingScale(7, boundingRect);
+        distance = circleRadius + boundingScale(2, boundingRect);
+        radius = distance + boundingScale(2, boundingRect)
+        arrowWidth = boundingScale(35, boundingRect)
+    } else {
+        pos = mapCoordinates(pos)
+        circleRadius = 7
+        distance = circleRadius + 2
+        radius = distance + 5;
+        arrowWidth = 35;
+    }
 
     if (dormant) {
         ctx.font = "20px Arial";
         ctx.textAlign = "center"
         ctx.fillStyle = fillStyle
-        ctx.fillText("?", offset_x, offset_y); 
+        ctx.fillText("?", pos.x, pos.y); 
     } else {
-        // Draw circle
 
+        // Draw circle
         ctx.beginPath();
-        ctx.arc(offset_x, offset_y, circleRadius, 0, 2 * Math.PI);
+        ctx.arc(pos.x, pos.y, circleRadius, 0, 2 * Math.PI);
         ctx.fillStyle = fillStyle;
         ctx.fill();
 
         if (hasBomb) {
             ctx.beginPath();
-            ctx.arc(offset_x, offset_y, circleRadius / 2, 0, 2 * Math.PI);
+            ctx.arc(pos.x, pos.y, circleRadius / 2, 0, 2 * Math.PI);
             ctx.fillStyle = "#dbb81d";
             ctx.fill();
         }
@@ -145,18 +260,16 @@ function drawEntity(x, y, fillStyle, dormant, hasBomb, yaw) {
         ctx.closePath();
 
         // Calculate arrowhead points
-        const distance = circleRadius + 2
-        const radius = distance + 5;
-        const arrowWidth = 35;
 
-        const arrowHeadX = offset_x + radius * Math.cos(yaw * (Math.PI / 180))
-        const arrowHeadY = offset_y - radius * Math.sin(yaw * (Math.PI / 180))
 
-        const arrowCornerX1 = offset_x + distance * Math.cos((yaw - arrowWidth) * (Math.PI / 180))
-        const arrowCornerY1 = offset_y - distance * Math.sin((yaw - arrowWidth) * (Math.PI / 180))
+        const arrowHeadX = pos.x + radius * Math.cos(yaw * (Math.PI / 180))
+        const arrowHeadY = pos.y - radius * Math.sin(yaw * (Math.PI / 180))
 
-        const arrowCornerX2 = offset_x + distance * Math.cos((yaw + arrowWidth) * (Math.PI / 180))
-        const arrowCornerY2 = offset_y - distance * Math.sin((yaw + arrowWidth) * (Math.PI / 180))
+        const arrowCornerX1 = pos.x + distance * Math.cos((yaw - arrowWidth) * (Math.PI / 180))
+        const arrowCornerY1 = pos.y - distance * Math.sin((yaw - arrowWidth) * (Math.PI / 180))
+
+        const arrowCornerX2 = pos.x + distance * Math.cos((yaw + arrowWidth) * (Math.PI / 180))
+        const arrowCornerY2 = pos.y - distance * Math.sin((yaw + arrowWidth) * (Math.PI / 180))
 
 
         const cicleYaw = 90-yaw
@@ -167,7 +280,7 @@ function drawEntity(x, y, fillStyle, dormant, hasBomb, yaw) {
 
         /// Backside of the arrow
         ctx.beginPath();
-        ctx.arc(offset_x, offset_y, distance, startAngle, endAngle)
+        ctx.arc(pos.x, pos.y, distance, startAngle, endAngle)
 
         /// Draw from corners to arrowhead
         ctx.lineTo(arrowCornerX1, arrowCornerY1);
@@ -279,6 +392,7 @@ addEventListener("DOMContentLoaded", (e) => {
     canvas = document.getElementById('canvas');
     canvas.width = 1024;
     canvas.height = 1024;
+    canvasAspectRatio = canvas.width / canvas.height
     ctx = canvas.getContext('2d');
 
     console.log(`[radarflow] connecting to ${websocketAddr}`)
